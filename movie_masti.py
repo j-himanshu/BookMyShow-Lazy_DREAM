86e0488c07886242ae83ee4c82cfb92616b7d71f
HIMANSHU JANAWADKAR <hs.9731391859@outlook.com>
2016-12-02 12:01:55 +0530

    Update movie_masti.py

--- movie_masti.py
+++ movie_masti.py
@@ -20,7 +20,7 @@ END
 """
 
 #type the movie_name and city
-movie_name = "kahaani 2"
+movie_name = "befikre"
 city = "bangalore"
 
 #CAUTION, FROM HERE ONWARDS, ONLY DEVELOPERS MAY PROCEED. USERS, PLEASE KEEP YOUR HANDS AWAY
@@ -30,8 +30,10 @@ from datetime import datetime
 #if stupid gives an upper case, to handle it!!
 city = city.lower()
 #what to search for
-search_string1 = "category\\/now showing\"},\"products\":[{\"name\":\"%s"%(movie_name)
-search_string2 = "title=\"%s"%(movie_name)
+search_string1 = "title=\"%s"%(movie_name)
+search_string2 = "category\\/now showing\"},\"products\":[{\"name\":\"%s"%(movie_name)
+
+found_url = False
 
 #the website where movie is displayed
 website = "https://in.bookmyshow.com/%s/movies/nowshowing"%(city)
@@ -41,15 +43,18 @@ while loop:
 	#Fetch the url
 	respons = urllib2.urlopen(website)
 	html_script = respons.read()
-	if search_string1.lower() in html_script.lower():
-		#Search the webpage
+	if found_url == False and search_string1.lower() in html_script.lower():		#Searching For Booking Link (search_string1)
 		for text in html_script.split("\n"):
-			if search_string2.lower() in text.lower():
-				loop = False
+			if search_string1.lower() in text.lower():
+				found_url = True													#Enabling search_string2 and disabling search_string1 
+				moviecode = text.split("href=")[1].split(">")[0].split("\"")[1]		#Fetch the booking link
+				booking_link = "https://in.bookmyshow.com%s"%(moviecode)
 				break
-
-moviecode = text.split("href=")[1].split(">")[0].split("\"")[1]		#Fetch the booking link
-booking_link = "https://in.bookmyshow.com%s"%(moviecode)
+	if found_url and search_string2.lower() in html_script.lower():					#Search for availibility provided booking link is present
+		loop = False
+		break
+	if found_url:
+		print "Movie Link- %s 	Bookings Not Open"%(booking_link)
 
 print "Book Tickets Now!\nBook Here @ ", booking_link
 webbrowser.open(booking_link)
